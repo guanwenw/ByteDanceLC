e628e1092f6061ad33954f6dfacd121c41df0e03
Jingwen Gu <carmangjw@gmail.com>
2021-02-16 01:16:09 -0500

    206 Reverse Linked List python solution

--- Solutions.py
+++ Solutions.py
@@ -2,7 +2,7 @@
 class Solution(object):
     def sortedSquares(self, nums):
          return sorted(x*x for x in nums)
-         
+
     def sortedSquares2(self, nums):
         """
         :type nums: List[int]
@@ -13,3 +13,24 @@ class Solution(object):
 
         nums.sort()
         return nums
+
+    # Definition for singly-linked list.
+    # class ListNode(object):
+    #     def __init__(self, val=0, next=None):
+    #         self.val = val
+    #         self.next = next
+
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        newHead = None
+        cur = head
+        while cur != None:
+            tmp = cur.next
+            cur.next = newHead
+            newHead = cur
+            cur = tmp
+
+        return newHead
