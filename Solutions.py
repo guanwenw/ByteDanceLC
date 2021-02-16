f4b57282d9baff516d74a1cee5c940d9ddd3ce7f
Jingwen Gu <carmangjw@gmail.com>
2021-02-16 00:35:33 -0500

    add solution

--- Solutions.py
+++ Solutions.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
+class Solution(object):
+    def sortedSquares(self, nums):
+         return sorted(x*x for x in nums)
+         
+    def sortedSquares2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            nums[i] = nums[i]*nums[i]
 
-def sortedSquares(self, nums):
-    """
-    :type nums: List[int]
-    :rtype: List[int]
-    """
-    for i in range(len(nums)):
-        nums[i] = nums[i]*nums[i]
-
-    nums.sort()
-    return nums
+        nums.sort()
+        return nums
